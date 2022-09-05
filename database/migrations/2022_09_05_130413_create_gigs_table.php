378f97610b1f3cf9e356c8beb958147b2eca1ada
peterdez <adebanjopete@gmail.com>
2022-09-05 14:27:41 +0100

    Create Gig, Company and Role models and migrations

---
+++ database/migrations/2022_09_05_130413_create_gigs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGigsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gigs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('salary');
+            $table->foreignId('company_id');
+            $table->foreignId('role_id');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gigs');
+    }
+}
